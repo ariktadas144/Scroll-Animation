3cbb09b7253ce4bbceec24a88e8c96bc4cdfbc79
Arpita Das <ariktadas144@gmail.com>
2026-03-01 16:27:26 +0530

    Updates

--- _next/static/ikDj-DCZABwewS94azSGz/_buildManifest.js
+++ _next/static/ikDj-DCZABwewS94azSGz/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Scroll-Animation//_next/:path+",
-        "destination": "/Scroll-Animation/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
